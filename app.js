8e9fb763ec9b686b6c68cd58742c65e63c988cd2
Victor <viclee02@gmail.com>
2018-03-15 23:44:04 -1000

    new exercises

---
+++ app.js
@@ -0,0 +1,55 @@
+/*Congratulations Prepsters, you've been selected for ANOTHER top secret mission (dare I say a sequel??). Should you choose to accept it, create a new branch and 
+do a git fetch and git pull to find your specific mission. After you complete your mission, commit your work and do a pull request.
+
+This repo will self-destruct in five seconds. Good luck (again)!!*/
+
+// Mission 1: 
+// Team Member: ?
+
+/*Create a function that will take in an array of numbers and return the sum of all even numbers below 12.*/
+
+@param {Array}
+@return {Number}
+
+var num = [1,2,3,4,5,6,8,9,10,12,14,16,18];
+
+// Mission 2: 
+// Team Member: ? 
+
+/*Create a function that will take in an array of numbers and return an array with each element half of its original value.*/
+
+@param {Array}
+@return {Array}
+
+var numberArr = [10, 20, 30];
+
+// Mission 3:
+// Team Member: ?
+
+/*Create a function that will iterate through the calendar array and return an array of all of the elements in a single array.*/
+
+@param {Array}
+@return {Array}
+
+var calendar = [["Jan", "MLK Day"], ["Feb", "Pres. Day"], ["Mar", "Good Fri"]];
+
+// Mission 4:
+// Team Member: ?
+
+/*Create a function that will take in an array of strings and return an array of string lengths.*/
+
+@param {Array}
+@return {Array}
+
+var nihongo = ["shinjirarenai", "hisashiburi", "otsukaresama", "itadakemasu", "homma"];
+
+
+
+
+
+
+
+
+
+
+
