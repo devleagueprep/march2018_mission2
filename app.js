e21f6fabddd9a7b2057c617615257cae1ae3378c
Victor <viclee02@gmail.com>
2018-03-15 23:46:14 -1000

    revised exercises

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ This repo will self-destruct in five seconds. Good luck (again)!!*/
 
 /*Create a function that will take in an array of numbers and return the sum of all even numbers below 12.*/
 
-@param {Array}
-@return {Number}
+// @param {Array}
+// @return {Number}
 
 var num = [1,2,3,4,5,6,8,9,10,12,14,16,18];
 
@@ -18,8 +18,8 @@ var num = [1,2,3,4,5,6,8,9,10,12,14,16,18];
 
 /*Create a function that will take in an array of numbers and return an array with each element half of its original value.*/
 
-@param {Array}
-@return {Array}
+// @param {Array}
+// @return {Array}
 
 var numberArr = [10, 20, 30];
 
@@ -28,8 +28,8 @@ var numberArr = [10, 20, 30];
 
 /*Create a function that will iterate through the calendar array and return an array of all of the elements in a single array.*/
 
-@param {Array}
-@return {Array}
+// @param {Array}
+// @return {Array}
 
 var calendar = [["Jan", "MLK Day"], ["Feb", "Pres. Day"], ["Mar", "Good Fri"]];
 
@@ -38,8 +38,8 @@ var calendar = [["Jan", "MLK Day"], ["Feb", "Pres. Day"], ["Mar", "Good Fri"]];
 
 /*Create a function that will take in an array of strings and return an array of string lengths.*/
 
-@param {Array}
-@return {Array}
+// @param {Array}
+// @return {Array}
 
 var nihongo = ["shinjirarenai", "hisashiburi", "otsukaresama", "itadakemasu", "homma"];
 
